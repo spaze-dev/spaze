a3be0398de0e4ca519b8578e5a9262d17156e09e
Vyntrix <frisch_valentin@icloud.com>
2024-09-19 23:13:14 +0200

    feat: :sparkles: open graph

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,27 @@
 <script lang="ts">
+  import SvelteSeo from "svelte-seo";
   import type { PageData } from "./$types";
   export let data: PageData;
 </script>
+
+<SvelteSeo 
+  title="SPAZE"
+  description="An open source and minimalistic link sharing app. Built with SvelteKit, Prisma, Lucia, and Tailwind CSS."
+  canonical="https://spaze.dev"
+  keywords="links, share, social, network, spaze"
+  openGraph={{
+    title: "SPAZE",
+    description: "An open source and minimalistic link sharing app. Built with SvelteKit, Prisma, Lucia, and Tailwind CSS.",
+    url: "https://spaze.dev",
+    site_name: "SPAZE",
+  }}
+  twitter={{
+    card: "summary_large_image",
+    site: "@vyntrix",
+    title: "SPAZE",
+    description: "An open source and minimalistic link sharing app. Built with SvelteKit, Prisma, Lucia, and Tailwind CSS.",
+  }}
+/>
 
 <div class="hero bg-base-100 h-full">
   <div class="hero-content text-center">
--- src/routes/[username]/+page.svelte
+++ src/routes/[username]/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import type { PageData } from "./$types";
+  import SvelteSeo from "svelte-seo";
   export let data: PageData;
 </script>
 
